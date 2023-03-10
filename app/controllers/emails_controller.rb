2848643df1f3e60f3c192fc688d70a92a5901656
Lionel Diot <diot.lionel2@gmail.com>
2023-03-10 07:55:01 +0100

    quick auto commit

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,6 +1,20 @@
 class EmailsController < ApplicationController
   before_action :set_email, only: %i[show edit update destroy]
 
+  def generate
+    @email = Email.new(object: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraph)
+
+    respond_to do |format|
+      if @email.save
+        format.html { redirect_to emails_path, notice: "Rendered in HTML." }
+        format.turbo_stream
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+
+  end
   def index
     @emails = Email.all.order(created_at: :desc)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :emails
-  
+  post '/generate', to: 'emails#generate', as: 'generate_email' 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root to: "emails#index"
   # Defines the root path route ("/")
