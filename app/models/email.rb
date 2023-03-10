a200624b7dafa47ca550d047555e508716fb1157
Lionel Diot <diot.lionel2@gmail.com>
2023-03-10 16:30:35 +0100

    quick auto commit

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,5 +1,5 @@
 class Email < ApplicationRecord
-  after_create_commit -> { broadcast_append_to "emails" }
+  after_create_commit -> { broadcast_prepend_to "emails" }
   after_update_commit -> { broadcast_replace_to "emails" }
   after_destroy_commit -> { broadcast_remove_to "emails" }
 end
